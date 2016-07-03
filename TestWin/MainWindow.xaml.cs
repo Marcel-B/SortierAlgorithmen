6c1cc2507439890b7e722775f2591dbf7469e853
marcelbenders <marcel.benders@outlook.de>
2016-07-03 18:02:32 +0200

    dth

--- TestWin/MainWindow.xaml.cs
+++ TestWin/MainWindow.xaml.cs
@@ -28,6 +28,7 @@ namespace TestWin
         Random rnd;
         bool set = false;
         List<double> flo;
+        List<DateTime> tt;
         public MainWindow()
         {
             InitializeComponent();
@@ -53,13 +54,31 @@ namespace TestWin
             if (!set)
             {
                 set = true;
+                // Enable MouseWheel Zooming
+                Pesgo.PeUserInterface.Scrollbar.MouseWheelFunction = MouseWheelFunction.HorizontalVerticalZoom;
+                Pesgo.PeUserInterface.Scrollbar.MouseDraggingX = true;  // note that pan gestures require MouseDragging to be enabled 
+                Pesgo.PeUserInterface.Scrollbar.MouseDraggingY = true;  // also, pan gestures should have ScrollingHorzZooom enabled
+
+                // Enable MouseWheel Zoom Smoothness
+                Pesgo.PeUserInterface.Scrollbar.MouseWheelZoomSmoothness = 5;
+                Pesgo.PeUserInterface.Scrollbar.PinchZoomSmoothness = 2;
+
+                // Allow zooming and panning //
+                Pesgo.PeUserInterface.Allow.Zooming = AllowZooming.HorzAndVert;
+                Pesgo.PeUserInterface.Scrollbar.ScrollingHorzZoom = true;
+                Pesgo.PeUserInterface.Scrollbar.ScrollingVertZoom = true;
+
+                // Enable ZoomWindow //
+                Pesgo.PePlot.ZoomWindow.Show = true;
+
+
                 Pesgo.PeString.MainTitle = "";
                 Pesgo.PeString.SubTitle = "";
                 Pesgo.PeString.XAxisLabel = "Zeit";
                 Pesgo.PeString.YAxisLabel = "Puls";
                 Pesgo.PeColor.QuickStyle = QuickStyle.DarkNoBorder;
                 Pesgo.PePlot.Option.BarGlassEffect = true;
-                Pesgo.PePlot.Method = GraphPlottingMethod.PointsPlusBestFitCurveGraphed;
+                Pesgo.PePlot.Method = SGraphPlottingMethod.PointsPlusLine;
 
                 Pesgo.PePlot.Option.GradientBars = 8;
                 Pesgo.PePlot.Option.LineShadows = true;
@@ -90,17 +109,29 @@ namespace TestWin
                 Pesgo.PePlot.Option.LineSymbolThickness = 3;
                 Pesgo.PePlot.Option.AreaBorder = 1;
                 Pesgo.PeUserInterface.Dialog.AllowSvgExport = true;
-
-
+                Pesgo.PeData.UsingXDataii = true;
+  
+                Pesgo.PeData.StartTime = tt[0].ToOADate();
                 for (int i = 0; i < flo.Count; i++)
                 {
                     Pesgo.PeData.Y[0, i] = (float)flo[i];
+                    Pesgo.PeData.Xii[0, i] = tt[i].ToOADate();
                     //Pesgo.PeData.Y[1, i] = (float)flo[i];
                 }
                 Pesgo.PeConfigure.RenderEngine = RenderEngine.Direct2D;
                 Pesgo.PeConfigure.AntiAliasGraphics = true;
                 Pesgo.PeConfigure.AntiAliasText = true;
 
+                Pesgo.PeGrid.Option.YearMonthDayPrompt = YearMonthDayPrompt.InsideTop;
+
+                // Optional related properties ...
+                // PeGrid.Option.TimeLabelType
+                // PeGrid.Option.DayLabelType
+                // PeGrid.Option.MonthLabelType
+                // PeGrid.Option.YearLabelType
+
+                // Enable DateTimeMode //
+                Pesgo.PeData.DateTimeMode = true;
                 Pesgo.PeFunction.ReinitializeResetImage();
             }
             Pesgo.Invalidate();
@@ -114,15 +145,20 @@ namespace TestWin
             ofd.ShowDialog();
             var path = ofd.FileName;
             double dd;
+            DateTime oo;
             flo = new List<double>();
+            tt = new List<DateTime>();
 
             var lines = File.ReadAllLines(path);
             foreach (string line in lines)
             {
                 var spl = line.Split(',');
                 var str = spl[2].Replace("\"","");
-                if (Double.TryParse(str, out dd))
+                if (double.TryParse(str, out dd))
                     flo.Add(dd);
+                str = spl[1].Replace("\"", "");
+                if (DateTime.TryParse(str, out oo))
+                    tt.Add(oo);
             }
 
             var foo = new Statistik.Statistik();
